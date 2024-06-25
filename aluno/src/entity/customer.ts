c4ec4c0287afc85f430f5591d2b17d142bdfaf71
Weslley-Prado <weslley.prado73@gmail.com>
2024-06-25 11:06:49 -0300

    feature/main: create test and class order service

--- aluno/src/entity/customer.ts
+++ aluno/src/entity/customer.ts
@@ -1,51 +1,62 @@
 import Address from "./address";
 
 export default class Customer {
-    private _id: string;
-    private _name: string;
-    private _address!: Address;
-    private _active: boolean = false;
+  private _id: string;
+  private _name: string;
+  private _address!: Address;
+  private _active: boolean = false;
+  private _rewardPoints: number = 0;
 
-    constructor(id: string, name: string) {
-        this._id = id;
-        this._name = name;
-        this.validate();
-    }
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+    this.validate();
+  }
 
-    get name(): string{
-        return this._name;
-    }
-    validate() {
-        if (this._name.length === 0) {
-            throw new Error("Name is required");
-        }
-        if(this._id.length === 0){
-            throw new Error("Id is required");
-        }
-    }
+  get id(): string {
+    return this._id;
+  }
+  get name(): string {
+    return this._name;
+  }
 
-    changeName(name: string) {
-        this._name = name;
-        this.validate();
+  get rewardPoints(): number {
+    return this._rewardPoints;
+  }
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
     }
-
-    isActive(): boolean{
-        return this._active
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
     }
+  }
 
-    activate() {
-        if(this._address === undefined){
-            throw new Error("Address is mandatory to activate a customer");
-        }
-        this._active = true;
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  isActive(): boolean {
+    return this._active;
+  }
+
+  activate() {
+    if (this._address === undefined) {
+      throw new Error("Address is mandatory to activate a customer");
     }
+    this._active = true;
+  }
 
-    deactivate() {
-        this._active = false;
-    }
+  deactivate() {
+    this._active = false;
+  }
 
-    set Address(address: Address){
-        this._address = address;
-    }
+  addRewardPoints(points: number) {
+    this._rewardPoints += points;
+  }
 
+  set Address(address: Address) {
+    this._address = address;
+  }
 }
